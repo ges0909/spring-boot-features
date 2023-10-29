a33f25e36c9a383f3365ed1e5f54f76ea9856aac
Gerrit Schrader <gerrit.schrader@gmail.com>
2023-10-29 09:52:18 +0100

    Cmdline runner run configuration stored

--- cmdline-runner/src/main/java/com/valantic/MainApplication.java
+++ cmdline-runner/src/main/java/com/valantic/MainApplication.java
@@ -2,7 +2,6 @@ package com.valantic;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 
 import java.util.stream.Stream;
 
@@ -14,10 +13,8 @@ public class MainApplication {
         final String command = Stream.of(args).findFirst().orElse("server");
         final String[] remainingArgs = Stream.of(args).skip(1).toArray(String[]::new); // left shift
         switch (command) {
-            // case "server" -> ServerApplication.main(remainingArgs);
-            // case "import" -> ImportApplication.main(remainingArgs);
-            case "server" -> SpringApplication.from(ServerApplication::main).run(remainingArgs);
-            case "import" -> SpringApplication.from(ImportApplication::main).run(remainingArgs);
+            case "server" -> ServerApplication.main(remainingArgs);
+            case "import" -> ImportApplication.main(remainingArgs);
             default -> LOGGER.error("Unknown command '{}'", command);
         }
     }
