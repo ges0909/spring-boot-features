a35db5758d4d5b47c6ac0995622fb192da1f80f1
GerritSchrader <Gerrit.Schrader-extern@deutschebahn.com>
2023-11-16 13:33:38 +0100

    Web layer tests refactored

--- web-layer-testing/src/test/java/com/valantic/HelloControllerTest.java
+++ web-layer-testing/src/test/java/com/valantic/HelloControllerTest.java
@@ -3,18 +3,23 @@ package com.valantic;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.nio.charset.StandardCharsets;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // start full Spring application context but without the server
-// @SpringBootTest
+// @SpringBootTest(classes = {HelloController.class})
 // @AutoConfigureMockMvc(addFilters = false) // injects 'MockMvc'
+// @EnableWebMvc
 
 // narrow the tests to only the web layer
-@WebMvcTest(controllers = {HelloController.class}, excludeFilters = {})
+@WebMvcTest(controllers = {HelloController.class})
 class HelloControllerTest {
 
     @Autowired
@@ -32,5 +37,18 @@ class HelloControllerTest {
         mockMvc.perform(get("/hello/Heike"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello, Heike"));
+    }
+
+    @Test
+    void testChangeInitialPassword() throws Exception {
+        mockMvc.perform(put("/isbpn-client-v1/bediener-verwaltung/initialpasswort-vergeben/{id}", 123L)
+                        .param("sessionId", "test")
+                        .param("benutzerkennung", "test")
+                        .param("bedienplatz", "test")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content("{\"password\": \"test\"}"))
+                .andExpect(status().isNotFound());
     }
 }
